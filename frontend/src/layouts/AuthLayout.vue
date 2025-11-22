4b922da33cdb92f8cebfafbfc1a9b1b8f89db53b
Marek Penzeš <marekpenzes3@gmail.com>
2025-11-22 15:07:19 +0100

    Reg a Login prerobeny na AuthLayout a 2 pages

---
+++ frontend/src/layouts/AuthLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>Chat Appus</q-toolbar-title>
+        <q-btn to="/" flat dense round icon="home" aria-label="Home" />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+//
+</script>
